a45bc1d58e61ebfbe6a99591b1ea624d8f44ae36
Arnob <arnobdam@gmail.com>
2022-11-01 16:49:39 -0400

    completed phase 2 / myreduce

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -6,10 +6,7 @@ Array.prototype.myEach = function (callback1) {
 
 Array.prototype.myMap = function (callback2) {
     let mappedArray = []
-    // return function() {
-    //     this.myEach(callback);
-    //     return mappedArray;
-    // }
+
     this.myEach(function (el) {
         mappedArray.push(callback2(el))
     })
@@ -20,5 +17,23 @@ function callback(num) {
     return num + 1;
 }
 
-let array = [1,2,3,4,5]
+let array = [1, 2, 3, 4, 5]
 console.log(array.myMap(callback))
+
+Array.prototype.myReduce = function (callback, acc) {
+
+    acc = acc || this.shift();
+
+    this.myEach(function (el) {
+        acc = callback(acc, el)
+    })
+
+    return acc;
+}
+
+function sum(x, y) {
+    return x + y;
+}
+
+console.log(array.myReduce(sum, 45))
+
